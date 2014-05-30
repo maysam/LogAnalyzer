ed295c20a8fc060cc400732c141747a6f36808e5
maysam <kherad@gmail.com>
2014-05-30 22:22:50 +0430

    code refactor -  making names more sensible

--- Application.vb
+++ Application.vb
@@ -110,15 +110,15 @@ Module Application
                             If IDmatch.Success Then
                                 PageID = IDmatch.Groups(2).Value
                                 If PageID <> "" Then
-                                Access_date = currentRow(0)
-                                time = currentRow(1)
-                                visitorIP = currentRow(10)
-                                Access_time = Convert.ToDateTime(Access_date + " " + time)
+                                    Access_date = currentRow(0)
+                                    time = currentRow(1)
+                                    visitorIP = currentRow(10)
+                                    Access_time = Convert.ToDateTime(Access_date + " " + time)
 
-                                'once we have pulled all th information we need from the logfile and we have tedermined it is one we want to youse we insert it into the database
+                                    'once we have pulled all th information we need from the logfile and we have tedermined it is one we want to youse we insert it into the database
 
-                                sqlCommand = New SqlCommand("INSERT INTO " + LogStorageTable + "([dateTimeAccessed],[IpAddress],[itemTYPE],[itemID]) Values('" + Access_time + "','" + visitorIP + "', '" + CorM + "','" + PageID + "') ", SQLConnection)
-                                sqlCommand.ExecuteNonQuery()
+                                    sqlCommand = New SqlCommand("INSERT INTO " + LogStorageTable + "([dateTimeAccessed],[IpAddress],[itemTYPE],[itemID]) Values('" + Access_time + "','" + visitorIP + "', '" + CorM + "','" + PageID + "') ", SQLConnection)
+                                    sqlCommand.ExecuteNonQuery()
                                 End If
                             End If
                         End If
@@ -168,12 +168,12 @@ Module Application
         'this is the table where you will store your results for the CIDs and under the current design the MIDs
         '#######################################################################
         Dim row As DataRow
-        Dim IpToGroup As New Dictionary(Of String, Integer)
-        Dim GroupToTime As New Dictionary(Of String, DateTime)
-        Dim GroupToVisited As New Dictionary(Of Integer, HashSet(Of Integer))
-        Dim GroupVisitCount As New Dictionary(Of Integer, Integer)
-        Dim CIDToCount As New Dictionary(Of Integer, Integer)
-        Dim CIDGroup_count As Integer = 0
+        Dim IPSession As New Dictionary(Of String, Integer)
+        Dim SessionTime As New Dictionary(Of String, DateTime)
+        Dim Session As New Dictionary(Of Integer, List(Of Integer))
+        Dim SessionCount As New Dictionary(Of Integer, Integer)
+        Dim ItemCount As New Dictionary(Of Integer, Integer)
+        Dim IndexCounter As Integer = 0
         Dim InfoTable = New DataTable
         Dim sqlCommand As New SqlCommand
         Dim closestMatches As New LinkedList(Of Tuple)
@@ -182,9 +182,6 @@ Module Application
         Dim rowEntropy As Double
         Dim totalEntropy As Double
         Dim resultsCount As Integer = 0
-
-        CIDGroup_count = 0
-
         '##########LOG TABLE CALL##############################
         Using da = New SqlDataAdapter("SELECT dateTimeAccessed, IpAddress, itemID FROM " & LogStorageTable & " where itemTYPE = '" & Model(0) & "' and dateTimeAccessed > '" & DateAdd(DateInterval.Month, -months_to_look_back, DateTime.Now) & "' order by dateTimeAccessed asc", SQLConnection)
             da.Fill(InfoTable)
@@ -192,68 +189,66 @@ Module Application
 
         For Each row In InfoTable.Rows
             'this is the check if the row should be sorted according to cid objects or mid objects
-            Dim _IpAddress = row.Item("IpAddress")
-            Dim _item As Integer = row.Item("itemID")
-            Dim within_timeframe As Boolean
-            Dim _IpToGroup As Integer = -1
-            If IpToGroup.ContainsKey(_IpAddress) Then
-                _IpToGroup = IpToGroup.Item(_IpAddress)
-                within_timeframe = DateDiff(DateInterval.Minute, GroupToTime.Item(_IpToGroup), row.Item("dateTimeAccessed")) <= 10080
-            End If
-            'this checks if the IpAddress has already been assigned to a group Id
-            If _IpToGroup <> -1 Then
+            Dim _IpAddress As String = row.Item("IpAddress")
+            Dim itemID As Integer = row.Item("itemID")
+            Dim index As Integer = -1
+            If IPSession.ContainsKey(_IpAddress) Then
+                index = IPSession.Item(_IpAddress)
                 'if exists check time to see if it is in the window to be a new group or appended to the current one
-                If within_timeframe Then
-                    GroupToTime.Item(_IpToGroup) = row.Item("dateTimeAccessed")
-                    GroupVisitCount.Item(_IpToGroup) += 1
-                    GroupToVisited.Item(_IpToGroup).Add(_item)
+                If DateDiff(DateInterval.Minute, SessionTime.Item(index), row.Item("dateTimeAccessed")) <= 10080 Then
+                    SessionTime.Item(index) = row.Item("dateTimeAccessed")
+                    SessionCount.Item(index) += 1
+                    Session.Item(index).Add(itemID)
                 Else
                     'Map this visit to a new group if it is outside the time window
-                    _IpToGroup = IpToGroup.Item(_IpAddress) = CIDGroup_count
-                    GroupToVisited.Add(CIDGroup_count, New HashSet(Of Integer)({_item}))
-                    GroupVisitCount.Add(CIDGroup_count, 1)
-                    GroupToTime.Add(CIDGroup_count, row.Item("dateTimeAccessed"))
-                    CIDGroup_count += 1
+                    index = IPSession.Item(_IpAddress) = IndexCounter
+                    Session.Add(IndexCounter, New List(Of Integer)({itemID}))
+                    SessionCount.Add(IndexCounter, 1)
+                    SessionTime.Add(IndexCounter, row.Item("dateTimeAccessed"))
+                    IndexCounter += 1
                 End If
             Else
                 'make new group as necessary
-                IpToGroup.Add(_IpAddress, CIDGroup_count)
-                GroupToVisited.Add(CIDGroup_count, New HashSet(Of Integer)({_item}))
-                GroupVisitCount.Add(CIDGroup_count, 1)
-                GroupToTime.Add(CIDGroup_count, row.Item("dateTimeAccessed"))
-                CIDGroup_count += 1
+                IPSession.Add(_IpAddress, IndexCounter)
+                Session.Add(IndexCounter, New List(Of Integer)({itemID}))
+                SessionCount.Add(IndexCounter, 1)
+                SessionTime.Add(IndexCounter, row.Item("dateTimeAccessed"))
+                IndexCounter += 1
             End If
         Next
         'freeing up no longer needed memory
-        GroupToTime.Clear()
-        IpToGroup.Clear()
+        SessionTime.Clear()
+        IPSession.Clear()
         InfoTable.Clear()
-        Dim SessionTable = "rSessions"
-        sqlCommand = New SqlCommand("Delete From " & SessionTable, SQLConnection)
-        sqlCommand.ExecuteNonQuery()
-        For Each key As Integer In GroupToVisited.Keys
-            sqlCommand = New SqlCommand("insert into " & SessionTable & " values ( " & key & ", " & GroupToVisited(key).Count & ",'" & String.Join(",", GroupToVisited(key)) & "')", SQLConnection)
-            sqlCommand.ExecuteNonQuery()
-        Next
 
-
-        Dim removableKeys = (From pair In GroupVisitCount Where pair.Value < 5 Select pair.Key).ToArray
+        Dim removableKeys = (From pair In Session Where pair.Value.Count < 5 Select pair.Key).ToArray
         For Each key In removableKeys
-            GroupVisitCount.Remove(key)
-            GroupToVisited.Remove(key)
+            SessionCount.Remove(key)
+            Session.Remove(key)
         Next
+
+        Dim SessionTable = "rSessions"
+        sqlCommand = New SqlCommand("Delete From " & SessionTable & " where model='" & Model(0) & "'", SQLConnection)
+        sqlCommand.ExecuteNonQuery()
+        For Each key As Integer In Session.Keys
+            For Each value In Session(key)
+                sqlCommand = New SqlCommand("insert into " & SessionTable & " values ( " & key + 1 & ", " & value & ", '" & Model(0) & "')", SQLConnection)
+                sqlCommand.ExecuteNonQuery()
+            Next
+        Next
+
         Dim PoolIDs As New Dictionary(Of Integer, Integer)
-        For Each key In GroupToVisited.Keys
-            Dim items = GroupToVisited(key)
+        For Each key In Session.Keys
+            Dim items = Session(key)
             For Each value In items
-                If CIDToCount.ContainsKey(value) Then
-                    CIDToCount.Item(value) += 1
+                If ItemCount.ContainsKey(value) Then
+                    ItemCount.Item(value) += 1
                 Else
-                    CIDToCount.Add(value, 1)
+                    ItemCount.Add(value, 1)
                 End If
             Next
         Next
-        Dim ResultsArray(CIDToCount.Count()) As Tuple(Of Double, Integer, Boolean)
+        Dim ResultsArray(ItemCount.Count()) As Tuple(Of Double, Integer, Boolean)
         Using da = New SqlDataAdapter("SELECT " & Model & "Id as ObjID, PoolId FROM t" & Model & " left join rcategories cat on constructid = cat.variableid", SQLConnection)
             da.Fill(InfoTable)
         End Using
@@ -263,19 +258,19 @@ Module Application
             End If
         Next
         InfoTable.Clear()
-        For Each key1 In CIDToCount.Keys()
+        For Each key1 In ItemCount.Keys()
             resultsCount = 0
             Dim PoolID = -1
             If PoolIDs.ContainsKey(key1) Then
                 PoolID = PoolIDs(key1)
             End If
-            For Each key2 In CIDToCount.Keys()
+            For Each key2 In ItemCount.Keys()
                 'get values for algorythm
                 If key1 <> key2 Then
-                    Dim Overlap_Count = GroupToVisited.Where(Function(obj) obj.Value.Contains(key1) And obj.Value.Contains(key2)).Count
-                    Dim FirstKeyOnly = GroupToVisited.Where(Function(obj) obj.Value.Contains(key1) And Not obj.Value.Contains(key2)).Count
-                    Dim SecondKeyOnly = GroupToVisited.Where(Function(obj) Not obj.Value.Contains(key1) And obj.Value.Contains(key2)).Count
-                    Dim NeitherKey = GroupToVisited.Where(Function(obj) Not obj.Value.Contains(key1) And Not obj.Value.Contains(key2)).Count
+                    Dim Overlap_Count = Session.Where(Function(obj) obj.Value.Contains(key1) And obj.Value.Contains(key2)).Count
+                    Dim FirstKeyOnly = Session.Where(Function(obj) obj.Value.Contains(key1) And Not obj.Value.Contains(key2)).Count
+                    Dim SecondKeyOnly = Session.Where(Function(obj) Not obj.Value.Contains(key1) And obj.Value.Contains(key2)).Count
+                    Dim NeitherKey = Session.Where(Function(obj) Not obj.Value.Contains(key1) And Not obj.Value.Contains(key2)).Count
 
                     rowEntropy = Entropy({Overlap_Count + SecondKeyOnly, FirstKeyOnly + NeitherKey})
                     columnEntropy = Entropy({Overlap_Count + FirstKeyOnly, SecondKeyOnly + NeitherKey})
